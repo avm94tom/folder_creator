f3014ae446561c9c0ae166063f2bda928c4bfb2a
Alex <30356166+avm94tom@users.noreply.github.com>
2017-09-02 14:23:40 +0300

    Add files via upload

---
+++ folder_creator.py
@@ -0,0 +1,37 @@
+import os
+path = r'C:\Users\Sony\Desktop\ALEX\Alex'
+#projectname = 'Python_projects' #folder named by default
+folders= \
+[['input' , [
+    ['src', []],
+    ['doc', []],
+]],
+ ['output', []],
+ ['scenes', []],
+ ['assets', [
+     ['textures',[]],
+     ['models', [
+         ['characters',[]],
+         ['locations', []]
+     ]],
+ ]]
+ ]
+
+
+def createFolder(path):
+    if not os.path.exists(path):
+       os.mkdir(path)
+
+def build(root, data):
+    if data:
+        for d in data:
+            name= d[0]
+            path=os.path.join(root, name)
+            createFolder(path)
+            build(path, d[1])
+
+projectname=raw_input('Enter project name: ')
+if projectname:
+    fullPath=os.path.join(path, projectname)
+    createFolder(fullPath)
+    build(fullPath, folders)
